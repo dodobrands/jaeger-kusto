c7b35b5f09dee9079289537c3911aa79a0b03b9a
ArtemTrofimushkin <artemtrofimushkin@gmail.com>
2022-02-13 22:59:11 +0300

    feat(*): implement graceful shutdown for server runner

--- runner/server.go
+++ runner/server.go
@@ -4,7 +4,13 @@ import (
 	"github.com/dodopizza/jaeger-kusto/config"
 	"github.com/hashicorp/go-hclog"
 	"github.com/jaegertracing/jaeger/plugin/storage/grpc/shared"
+	"google.golang.org/grpc"
+	"io"
 	"net"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
 )
 
 func serveServer(c *config.PluginConfig, store shared.StoragePlugin, logger hclog.Logger) error {
@@ -29,9 +35,36 @@ func serveServer(c *config.PluginConfig, store shared.StoragePlugin, logger hclo
 	}
 
 	logger.Info("starting server on address", "address", listener.Addr())
+	wg := registerGracefulShutdown(server, store, logger)
 	if err := server.Serve(listener); err != nil {
 		return err
 	}
 
+	wg.Wait()
 	return nil
+}
+
+func registerGracefulShutdown(server *grpc.Server, store shared.StoragePlugin, logger hclog.Logger) *sync.WaitGroup {
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, os.Interrupt, syscall.SIGTERM)
+
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+
+	go func() {
+		sig := <-signals
+		logger.Info("received signal, attempting gracefully stop server and plugin", "signal", sig)
+		server.GracefulStop()
+
+		// perform cleanup logic on writer
+		c, ok := store.SpanWriter().(io.Closer)
+		if ok {
+			_ = c.Close()
+		}
+
+		logger.Info("server stopped")
+		wg.Done()
+	}()
+
+	return wg
 }
